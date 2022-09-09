45eca52aa700f5796451ed7108ea078cd22d8608
betomelo_9 <betomelo_9@hotmail.com>
2022-09-08 21:11:48 -0300

    Primeira versao

---
+++ FilaPoc/FilaEstatica.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaPoc
+{
+	public class FilaEstatica : IFila<int>
+	{
+		private int?[] VetorDeElementos { get; set; }
+		public int? Inicio { get; private set; }
+		public int? Fim { get; private set; }
+		public int Qtde { get; private set; }
+		public int Tamanho { get; private set; }
+
+		public FilaEstatica(int tamanho)
+		{
+			Inicio = null;
+			Fim = null;
+			Qtde = 0;
+			Tamanho = tamanho;
+			VetorDeElementos = new int?[tamanho];
+
+		}
+		public int Desenfilerar()
+		{
+			if(EstaVazia()) throw new Exception("UnderFlow");
+
+			int valorRetorno = VetorDeElementos[Inicio.Value].Value;
+			
+			VetorDeElementos[Inicio.Value] = null;
+			Qtde--;
+			if (EhUltimaPosicaoInicio()) Inicio = 0;
+			else Inicio++;
+
+			if (EstaVazia()) { Inicio = null; Fim = null; }
+
+
+			return valorRetorno;
+		}
+
+		public void Enfilerar(int item)
+		{
+			if (EstaCheia())
+				throw new Exception("OverFlow");
+
+			if (EstaVazia())
+			{
+				Inicio = 0;
+				Fim = 0;
+			}
+			else {
+				if (EhUltimaPosicaoFim()) Fim = 0;	
+				else Fim++; 
+			}
+
+			VetorDeElementos[Fim.Value] = item;
+			Qtde++;
+
+		}
+
+		public bool EstaVazia() => Qtde == 0;
+
+		public bool EstaCheia() => Qtde == Tamanho;
+
+		public bool EhUltimaPosicaoFim() => Fim == (Tamanho-1);
+		public bool EhUltimaPosicaoInicio() => Inicio == (Tamanho-1);
+
+	}
+}
---
+++ FilaPoc/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FilaPoc;
+
+Console.WriteLine("Hello, World!");
+FilaEstatica filaEstatica = new FilaEstatica(5);
+filaEstatica.Enfilerar(10);
+filaEstatica.Enfilerar(20);
+filaEstatica.Enfilerar(30);
+filaEstatica.Enfilerar(40);
+filaEstatica.Enfilerar(50);
+filaEstatica.Desenfilerar();
+filaEstatica.Enfilerar(60);
+filaEstatica.Desenfilerar();
+filaEstatica.Desenfilerar();
+filaEstatica.Desenfilerar();
+filaEstatica.Desenfilerar();
+filaEstatica.Enfilerar(70);
+
+
